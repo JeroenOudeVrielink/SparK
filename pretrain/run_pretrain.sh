d0406a16e784315ed3be130a2de38be874731eac
JeroenOudeVrielink <jeroenov98@gmail.com>
2024-01-05 16:27:55 +1030

    chaning name

--- pretrain/run_pretrain.sh
+++ pretrain/run_pretrain.sh
@@ -1,6 +1,6 @@
 
 python main.py \
---exp_name=base_params_bs64_ep400 \
+--exp_name=spark_base_params_bs64_ep400 \
 --exp_dir=/media/jvrielink/9958194f-e772-4a20-b156-50f2ac51f106/spark_data \
 --data_path=/home/jvrielink/AIML_rot_corrected \
 --model=resnet50 \
