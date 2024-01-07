bf1e4d02de1449ecd8baf011555123af5c9b2c68
JeroenOudeVrielink <jeroenov98@gmail.com>
2024-01-08 10:20:45 +1030

    add docker run file

---
+++ docker_ws3_gpu1.sh
@@ -0,0 +1,9 @@
+# docker run --gpus all -v $pwd -w /code -it pytorch-test
+docker run \
+-it \
+--rm \
+-v $(pwd):/code \
+--gpus '"device=1"' \
+--mount type=bind,src=/home/jvrielink,target=/jvrielink \
+--shm-size 64G \
+jvrielink/pytorch_spark
