e614c3c278b0bf4b7b5414a01b32308e2dd2b2d4
JeroenOudeVrielink <jeroenov98@gmail.com>
2024-01-08 11:42:26 +1030

    increase num workers

--- pretrain/run_pretrain.sh
+++ pretrain/run_pretrain.sh
@@ -1,10 +1,11 @@
 
 python main.py \
 --exp_name=spark_base_params_bs256_ep400 \
---exp_dir=jvrielink/data/spark_models \
+--exp_dir=/jvrielink/data/spark_models \
 --data_path=/jvrielink/AIML_rot_corrected \
 --model=resnet50 \
 --bs=256 \
---ep=400
+--ep=400 \
+--dataloader_workers=16
 
 
