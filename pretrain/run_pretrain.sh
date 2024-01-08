b3dfcf74b227602155f16ee3b28cccf9f61ae5dd
JeroenOudeVrielink <jeroenov98@gmail.com>
2024-01-08 10:41:31 +1030

    adding mini annotations

--- pretrain/run_pretrain.sh
+++ pretrain/run_pretrain.sh
@@ -1,9 +1,18 @@
 
+# python main.py \
+# --exp_name=spark_base_params_bs64_ep400 \
+# --exp_dir=jvrielink/data/spark_models \
+# --data_path=/jvrielink/AIML_rot_corrected \
+# --model=resnet50 \
+# --bs=64 \
+# --ep=400
+
 python main.py \
---exp_name=spark_base_params_bs64_ep400 \
---exp_dir=/media/jvrielink/9958194f-e772-4a20-b156-50f2ac51f106/spark_data \
---data_path=/home/jvrielink/AIML_rot_corrected \
+--exp_name=debug \
+--exp_dir=jvrielink/data/spark_models \
+--data_path=/jvrielink/AIML_rot_corrected \
 --model=resnet50 \
---bs=64 \
---ep=400
+--bs=256 \
+--ep=2
+
 
