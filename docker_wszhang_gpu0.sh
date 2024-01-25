fadf4451d67643082355cb80cb3e24b6ab2cca18
JeroenOudeVrielink <jeroenov98@gmail.com>
2024-01-25 11:12:03 +1030

    update run docker

--- docker_wszhang_gpu0.sh
+++ docker_wszhang_gpu0.sh
@@ -4,7 +4,7 @@ docker run \
 --rm \
 -v $(pwd):/code \
 --gpus '"device=0"' \
---mount type=bind,src=/home/zhibin,target=/jvrielink \
+--mount type=bind,src=/home/zhibin/data,target=/jvrielink \
 --mount type=bind,src=/home/zhibin/data/spark_models,target=/spark_models \
 --shm-size 64G \
 jvrielink/pytorch_spark
