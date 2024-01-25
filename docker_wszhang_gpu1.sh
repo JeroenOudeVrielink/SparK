b92e87fbca6009ff2bfd08195c53d7433a2ed546
JeroenOudeVrielink <jeroenov98@gmail.com>
2024-01-25 15:20:49 +1030

    adding docker gpu1

---
+++ docker_wszhang_gpu1.sh
@@ -0,0 +1,10 @@
+# docker run --gpus all -v $pwd -w /code -it pytorch-test
+docker run \
+-it \
+--rm \
+-v $(pwd):/code \
+--gpus '"device=1"' \
+--mount type=bind,src=/home/zhibin/data,target=/jvrielink \
+--mount type=bind,src=/home/zhibin/data/spark_models,target=/spark_models \
+--shm-size 64G \
+jvrielink/pytorch_spark
