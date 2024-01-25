5fd6f29ce8964864f27c89b72f36606266f79476
JeroenOudeVrielink <jeroenov98@gmail.com>
2024-01-25 10:57:06 +1030

    adding run docker for zhang

---
+++ docker_wszhang_gpu0.sh
@@ -0,0 +1,10 @@
+# docker run --gpus all -v $pwd -w /code -it pytorch-test
+docker run \
+-it \
+--rm \
+-v $(pwd):/code \
+--gpus '"device=0"' \
+--mount type=bind,src=/home/zhibin,target=/jvrielink \
+--mount type=bind,src=/home/zhibin/data/spark_models,target=/spark_models \
+--shm-size 64G \
+jvrielink/pytorch_spark
--- pretrain/run_pretrain_laplace.sh
+++ pretrain/run_pretrain_laplace.sh
@@ -4,9 +4,9 @@ python main.py \
 --exp_dir=/spark_models \
 --data_path=/jvrielink/AIML_rot_corrected \
 --model=resnet50 \
---bs=256 \
---ep=300 \
---dataloader_workers=14 \
+--bs=32 \
+--ep=301 \
+--dataloader_workers=8 \
 --mask=0.75 \
 --laplace_recon=True \
 --input_size=448
