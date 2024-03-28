cf553df15c3787f13af9bbb415fb6f931e769265
JeroenOudeVrielink <jeroenov98@gmail.com>
2024-03-28 16:41:49 +1030

    decrease bs

--- pretrain/run_pretrain_weighted_masking.sh
+++ pretrain/run_pretrain_weighted_masking.sh
@@ -2,11 +2,11 @@
 
 
 python main.py \
---exp_name=spark_in224_bs128_weighted_masking \
+--exp_name=spark_in224_bs64_weighted_masking \
 --exp_dir=/spark_models \
 --data_path=/jvrielink/AIML_rot_corrected \
 --model=resnet50 \
---bs=128 \
+--bs=64 \
 --ep=301 \
 --dataloader_workers=12 \
 --mask=0.6 \
